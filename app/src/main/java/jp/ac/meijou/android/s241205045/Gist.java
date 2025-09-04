f9f6a22ea16b42582742b28bd5c501d7a40d87b3
Takuma Oguri <183122712+ogu618@user.noreplay.github.com>
2025-09-04 12:20:03 +0900

    day4 2限 Okhttp Gist WebAPI

---
+++ app/src/main/java/jp/ac/meijou/android/s241205045/Gist.java
@@ -0,0 +1,12 @@
+package jp.ac.meijou.android.s241205045;
+
+import java.util.Map;
+
+public class Gist {
+    public Map<String, GistFile> files;
+
+    public static class GistFile {
+        public String content;
+    }
+
+}
