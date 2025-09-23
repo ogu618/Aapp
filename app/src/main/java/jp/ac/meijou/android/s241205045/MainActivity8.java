55547fd75458c969f4c34f220f4c6abba09c2906
Takuma Oguri <183122712+ogu618@user.noreplay.github.com>
2025-09-23 17:17:16 +0900

    9/23
    build.gradleにTensorFlow Lite のライブラリを追加

--- app/src/main/java/jp/ac/meijou/android/s241205045/MainActivity8.java
+++ app/src/main/java/jp/ac/meijou/android/s241205045/MainActivity8.java
@@ -1,6 +1,10 @@
 package jp.ac.meijou.android.s241205045;
 
+import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,13 +12,29 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import jp.ac.meijou.android.s241205045.databinding.ActivityMain7Binding;
+import jp.ac.meijou.android.s241205045.databinding.ActivityMain8Binding;
+
 public class MainActivity8 extends AppCompatActivity {
+
+    private ActivityMain8Binding binding;
+    private ConnectivityManager connectivityManager;
+
+    private ImageView inputImage;
+    private Button processButton;
+    private TextView resultText;
+
+    private ImageClassifier imageClassifier;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main8);
+//        setContentView(R.layout.activity_main8);
+
+        binding = ActivityMain8Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
